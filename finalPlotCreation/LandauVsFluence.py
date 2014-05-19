8f53e7862415bf6086e3f36a7912250293d6f253
Felix Bachmair <bachmair@phys.ethz.ch>
2014-05-19 15:01:06 +0000

    added final plot creation module
    git-svn-id: svn+ssh://utka42.ethz.ch/rd42/trunk@1111 5e05d84c-b09a-4460-8136-89425579e6ea

---
+++ finalPlotCreation/LandauVsFluence.py
@@ -0,0 +1,93 @@
+import ConfigParser
+import ROOT
+from ROOT import TCanvas
+import utilities
+
+class LandauVsFluence():
+    def __init__(self,config=None):
+        self.plots ={}
+        self.canvas = None
+        self.config = config
+
+        self.set_runnos()
+        self.set_conversions()
+        self.set_irradiations()
+        pass
+    def set_runnos(self):
+        if not self.config:
+            self.runnos = [172080]
+            return
+        runs = sorted([int(i) for i in self.config.options('Runs')])
+        runnos = [self.config.getint('Runs','%d'%x) for x in runs]
+        self.runnos = runnos
+        print 'runnos: ',self.runnos
+    def set_conversions(self):
+        self.conversions={}
+        for run in self.runnos:
+            runno = '%d'%run
+            runno = runno[:3]
+            if self.config:
+                if self.config.has_option('Conversion',runno):
+                    conv = self.config.getfloat('Conversion',runno)
+                elif self.config.has_option('Conversion','default'):
+                    conv = self.config.getfloat('Conversion','default')
+                else:
+                    conv = 7.6
+            else:
+                conv = 7.6
+            self.conversions[runno] = conv
+        print 'conversions',self.conversions
+    def set_irradiations(self):
+        pass
+
+
+
+
+    def get_conversion(self,runno):
+        runno = '%d'%runno
+        runno = runno[:3]
+        return    self.conversions[runno]/100.
+        
+    def create_plot(self):
+        self.plots ={}
+        c1 = TCanvas()
+        for runno in self.runnos:
+            if self.config:
+                name = self.config.get('Main','histoName')
+            else:
+                name = 'hDiaTranspAnaPulseHeightOf2HighestIn10Strips'
+
+            rundes =''
+            plot = utilities.get_plot(config,runno,rundes,'hDiaTranspAnaPulseHeightOf2HighestIn10Strips')
+            plot.SetName(str(plot.GetName)+'%s'%runno)
+            newPlot = utilities.convert_x_to_electrons(plot,self.get_conversion(runno))
+            if self.config and self.config.has_option('Plot','XTitle'):
+                newPlot.GetXaxis().SetTitle(self.config.get('Plot','XTitle'))
+            if self.config and self.config.has_option('Plot','YTitle'):
+                newPlot.GetYaxis().SetTitle(self.config.get('Plot','YTitle'))
+            if self.config and self.config.has_option('Plot','Title'):
+                newPlot.SetTitle(self.config.get('Plot','Title') + ' %s'%runno )
+            self.plots[runno] = newPlot
+            c1.cd()
+            index = self.runnos.index(runno)
+            color = utilities.get_color(index)
+            newPlot.SetLineColor(color)
+            if index==0:
+                newPlot.DrawNormalized()
+            else:
+                newPlot.DrawNormalized('same')
+        c1.Update()
+        self.canvas = c1
+                
+
+
+        
+
+if __name__ == "__main__":
+
+    config = ConfigParser.ConfigParser()
+    config.read('config.cfg')
+
+
+    landau = LandauVsFluence(config)
+    landau.create_plot()
---
+++ finalPlotCreation/utilities.py
@@ -0,0 +1,67 @@
+import ROOT
+from ROOT import TCanvas
+
+def get_file(config,runno,rundes):
+    print 'get file ',runno,rundes
+    path  = '.'
+    if config and config.has_option('Main','Path'):
+        path = config.get('Main','Path')
+    path += '/%d/'%runno
+    if any(x in rundes for x in ['left','right']):
+        path+='%s/'%rundes
+    
+    if config and config.has_option('Main','relPath'):
+        path += config.get('Main','relPath')+'/'
+
+    if config and config.has_option('Main','fileName'):
+        path += config.get('Main','fileName')
+    else:
+        path += 'histograms.root'
+    print path
+    raw_input()
+
+    f =ROOT.TFile('histograms.root')
+    return f
+
+def get_color(index):
+    return index
+    
+def get_plot(config,runno,rundes,name):
+
+
+    newKey = ''
+    f = get_file(config,runno,rundes)
+    for key in f.GetListOfKeys():
+        c = f.FindObjectAny(key.GetName())
+        if c == None:
+            continue
+        if type(c) == ROOT.TCanvas:
+            h = c.GetPrimitive(name)
+        else:
+            h = c
+        if type(h) == None:
+            continue
+        if h == None:
+            continue
+        if h.GetName()==name:
+            return h
+    return 0
+
+def convert_x_to_electrons(plot,conversion):
+    name = plot.GetName()
+    name+='_electrons'
+    title = plot.GetTitle()
+    title+= ' in electrons'
+    newPlot = ROOT.TH1F(name,title,plot.GetNbinsX(),plot.GetXaxis().GetXmin()/conversion,plot.GetXaxis().GetXmax()/conversion)
+    for bin in range(0,plot.GetNbinsX()+1):
+        content = plot.GetBinContent(bin)
+        newPlot.SetBinContent(bin,content)
+    newPlot.SetEntries(plot.GetEntries())
+    return newPlot
+
+    pass
+
+if __name__ == "__main__":
+    name = 'hDiaTranspAnaPulseHeightOf2HighestIn10Strips'
+    plot = get_plot(1,name)
+    newPlot = convert_x_to_electrons(plot,6.8/100.)
